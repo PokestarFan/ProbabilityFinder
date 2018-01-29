49f65f5bcdf885aeb70b456f8d93a4b90a91b0c4
PokestarFan <azoysarkar@gmail.com>
2018-01-29 17:02:16 -0500

    start argparse

--- probability.py
+++ probability.py
@@ -81,3 +81,6 @@ def get_input(times = True, odds = True):
     if odds:
         odds = input('The odds?')
     run_tests(times, odds)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run big batch tests to really find out probability.')
