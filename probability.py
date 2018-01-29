06e638ee777a5a8a1fc0d9e76e4443785ce68469
PokestarFan <azoysarkar@gmail.com>
2018-01-28 19:14:26 -0500

    Im done

---
+++ probability.py
@@ -0,0 +1,68 @@
+from random import randrange
+import os
+
+
+def write_to_csv(filename, *args, newline = True):
+    write_string = ''
+    for arg in args:
+        if type(arg) == list:
+            for i in arg:
+                write_string += str(i) + ','
+        else:
+            write_string += str(arg) + ','
+    if newline:
+        write_string = write_string.rstrip(',') + '\n'
+    else:
+        write_string = write_string.rstrip(',')
+    with open(filename+'.csv', 'a') as file:
+        file.write(write_string)
+
+def move_dir(dirname, parent = False):
+    if not parent:
+        dirname = str(dirname)
+        exists = os.path.isfile(dirname)
+        try:
+            os.mkdir(dirname)
+            os.chdir(dirname)
+        except FileExistsError:
+            os.chdir(dirname)
+    else:
+        os.chdir("..")
+
+def calculate_probability(odds):
+    file_count = 0
+    move_dir('Probability')
+    move_dir(str(odds))
+    d = {}
+    writelist = []
+    percentlist = []
+    for i in range(odds):
+        d[str(i)] = 0
+        writelist.append(f'Times {i}')
+        percentlist.append(f'Percent {i}')
+    while True:
+        if os.path.isfile(str(file_count)+'.csv'):
+            file_count += 1
+        else:
+            break
+    filename = str(file_count)
+    write_to_csv(filename, 'Number', 'Value')
+    for i in range(1000):
+        ran = randrange(odds)
+        ran = int(ran)
+        d[str(ran)] += 1
+        if i == 999:
+            write_to_csv(filename, i, ran+1, newline = False)
+        else:
+            write_to_csv(filename, i, ran+1)
+    writelist2 = []
+    for i in d:
+        writelist2.append(d[i])
+    percentlist2 = []
+    for i in writelist2:
+        percentlist2.append(round(((i/1000)*100), 2))
+    if os.path.isfile('runs.csv'):
+        write_to_csv('runs', file_count, writelist2, percentlist2)
+    else:
+        write_to_csv('runs', 'Run #', writelist, percentlist)
+        write_to_csv('runs', file_count, writelist2, percentlist2)
