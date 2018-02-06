55d7b6b056d7bfa02785e544fd1c38bcd7bc28ab
PokestarFan <azoysarkar@gmail.com>
2018-02-05 19:21:10 -0500

    adding data

--- fun.py
+++ fun.py
@@ -3,7 +3,7 @@ from subprocess import run, PIPE
 
 for i in tqdm(range(1000)):
 	try:
-		run('cmd.exe /C probability.py {} --no_print'.format(i+2))
+		run('cmd.exe /C probability.py {} --no_print --low_cpu 1'.format(i+2))
 	except KeyboardInterrupt:
 		print('User quit program')
 		exit()
